d7c89279037b49e48c5cf60a982c649ecc7bcbcb
ocoa <shawn@ocoa.com>
2018-08-20 17:16:26 -0400

    ...

---
+++ main/recipes/setup.rb
@@ -0,0 +1,27 @@
+conf_available_dir = node[:apache][:conf_available_dir]
+
+node[:deploy].each do |app_name, deploy|
+	
+	#Base Server Setup
+	script "base_setup" do
+		not_if { ::File.exists?("/root/mkswap.txt") }
+	  interpreter "bash"
+	  user "root"
+	  cwd "/root"
+	  code <<-EOH
+			
+			chkconfig ntpd on
+			service ntpd start
+			
+			# Add Swap
+			touch /root/mkswap.txt
+      /bin/dd if=/dev/zero of=/mnt/swapfile bs=1M count=2048
+			chown root:root /mnt/swapfile
+			chmod 600 /mnt/swapfile
+			/sbin/mkswap /mnt/swapfile
+			/sbin/swapon /mnt/swapfile
+			
+	  EOH
+	end
+
+end
---
+++ storage/recipes/setup.rb
@@ -0,0 +1,4 @@
+#Chef::Log.info('HELLO')
+#Chef::Log.info(node[:database][:host])
+#yml_string = YAML::dump(node[:apache].to_hash)
+#Chef::Log.info(yml_string)
