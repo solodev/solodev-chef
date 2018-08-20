badde8c66c1c8e8af10f4aa4466340166116470e
ocoa <shawn@ocoa.com>
2018-08-20 16:07:33 -0400

    ..

--- main/recipes/client_settings.rb
+++ main/recipes/client_settings.rb
@@ -2,8 +2,6 @@ document_root = node[:install][:document_root]
 software_name = node[:install][:software_name]
 client_name = node[:install][:client_name]
 
-instance = search("aws_opsworks_instance", "self:true").first # this gets the databag for the instance
-
 template 'Client_Settings.xml' do
 	not_if { ::File.exists?("#{document_root}/#{software_name}/clients/#{client_name}/Client_Settings.xml") }
   path "#{document_root}/#{software_name}/clients/#{client_name}/Client_Settings.xml"
