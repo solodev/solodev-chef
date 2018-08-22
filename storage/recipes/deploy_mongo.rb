bacb1d3db823aed4cc45aceb3b8ad1ff08bd48af
ocoa <shawn@ocoa.com>
2018-08-22 12:21:08 -0400

    ..

--- storage/recipes/deploy_mongo.rb
+++ storage/recipes/deploy_mongo.rb
@@ -21,3 +21,23 @@ template 'mongod.conf' do
   group 'root'
   mode 0644
 end
+
+directory '/mongo/data' do
+	not_if do ::File.exists?("/mongo/data") end
+  owner 'mongod'
+  group 'mongod'
+  mode '0755'
+  recursive true
+  ignore_failure true
+  action :create
+end
+
+directory '/mongo/log' do
+	not_if do ::File.exists?("/mongo/log") end
+  owner 'mongod'
+  group 'mongod'
+  mode '0755'
+  recursive true
+  ignore_failure true
+  action :create
+end
