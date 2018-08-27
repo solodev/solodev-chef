c2ff9a808f0cada21ceab34d7ccf593bc090ac56
ocoa <shawn@ocoa.com>
2018-08-27 10:39:06 -0400

    fix names

--- mongo/metadata.rb
+++ mongo/metadata.rb
@@ -1,1 +1,1 @@
-name 'storage'
+name 'mongo'
--- solodev/metadata.rb
+++ solodev/metadata.rb
@@ -1,4 +1,1 @@
-#http://berkshelf.com/
-#https://docs.aws.amazon.com/opsworks/latest/userguide/best-practices-packaging-cookbooks-locally.html
-
-name 'main'
+name 'solodev'
