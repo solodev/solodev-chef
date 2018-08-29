ee332658cc2596bad409080dce65e3838017624c
ocoa <shawn@ocoa.com>
2018-08-29 13:23:48 -0400

    ..

--- mongo/recipes/deploy_mongo_single.rb
+++ mongo/recipes/deploy_mongo_single.rb
@@ -1,12 +1,12 @@
 DBUSER = node[:install][:DBUSER]
 DBPASSWORD = node[:install][:DBPASSWORD]
 
-script "mongo_user" do
+script "init_mongo" do
 	not_if { ::File.exists?('/root/initmongo.txt') }
-  interpreter "bash"
-  user "root"
-  cwd "/root"
-  code <<-EOH
+	interpreter "bash"
+	user "root"
+	cwd "/root"
+	code <<-EOH
 
 		touch /root/initmongo.txt
 		echo 'rs.initiate();' >> /root/initmongo.js
