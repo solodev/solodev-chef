85ce8150c73fb1378ac11c9b504dcfa5903b3892
ocoa <shawn@ocoa.com>
2018-11-19 13:15:10 -0500

    ..

--- solodev/recipes/deploy_solodev.rb
+++ solodev/recipes/deploy_solodev.rb
@@ -65,6 +65,11 @@ script "install_software" do
 		mv solodev/license.txt #{DocumentRoot}/#{SoftwareName}/
 		rm -Rf /root/solodev
 
+		#Add PEM
+		mkdir -p #{DocumentRoot}/#{SoftwareName}/clients/solodev/jwt
+		openssl genrsa -passout pass:ocoa -out #{DocumentRoot}/#{SoftwareName}/clients/solodev/jwt/private.pem 4096
+		openssl rsa -pubout -passin pass:ocoa -in #{DocumentRoot}/#{SoftwareName}/clients/solodev/jwt/private.pem -out #{DocumentRoot}/#{SoftwareName}/clients/solodev/jwt/public.pem
+
 		service httpd start
 		if [ -f /etc/init.d/php72-php-fpm ]; then
 			service php72-php-fpm start
