d6af831c02194df44e655b196b59d59a66db8622
ocoa <shawn@ocoa.com>
2019-03-15 08:13:52 -0400

    add new settings file

--- solodev/recipes/client_settings.rb
+++ solodev/recipes/client_settings.rb
@@ -3,13 +3,13 @@ SoftwareName = node[:install][:SoftwareName]
 ClientName = node[:install][:ClientName]
 MongoHost = node[:install][:MongoHost]
 
-template 'Client_Settings.xml' do
-	not_if { ::File.exists?("#{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}/Client_Settings.xml") }
-  path "#{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}/Client_Settings.xml"
+template '.env' do
+	not_if { ::File.exists?("#{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}/.env") }
+  path "#{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}/.env"
   variables( 
   	MongoHost: "#{MongoHost}"
   )
   action :create
-  source 'Client_Settings.xml.erb'
+  source 'client.env.erb'
   mode '0755'
 end
