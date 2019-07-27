1a351056dae792f6db630d6785e09c31b547d4d6
techcto <smoore@solodev.com>
2019-07-27 13:11:24 -0400

    restarted was failing

--- solodev/recipes/deploy_solodev.rb
+++ solodev/recipes/deploy_solodev.rb
@@ -120,8 +120,8 @@ script "restart_web" do
     code <<-EOH
 		mkdir -p #{DocumentRoot}/#{SoftwareName}/tmp
 		chmod 777 #{DocumentRoot}/#{SoftwareName}/tmp
-        if [ -f /etc/init.d/httpd ]; then
-            service httpd restart
-        fi
+        # if [ -f /etc/init.d/httpd ]; then
+        #     service httpd restart
+        # fi
     EOH
 end
