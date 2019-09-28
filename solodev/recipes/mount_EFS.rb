5bfb2f84faa57927e045a333686cca3cd9543c01
techcto <smoore@solodev.com>
2019-09-28 10:54:26 -0400

    update permissions

--- solodev/recipes/mount_EFS.rb
+++ solodev/recipes/mount_EFS.rb
@@ -20,8 +20,8 @@ script "mount_client" do
 	mount -t nfs4 -o nfsvers=4.1 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).#{EFSHost}.efs.#{Region}.amazonaws.com:/ #{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}     
 	echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).#{EFSHost}.efs.#{Region}.amazonaws.com:/ #{DocumentRoot}/#{SoftwareName}/clients/#{ClientName} nfs defaults 0 0" >> /etc/fstab
 	
-	chown -Rf apache.apache #{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}
-	chmod -Rf 2770 #{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}
+	chown -f apache.apache #{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}
+	chmod -f 2770 #{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}
 	
 	touch /home/ec2-user/efs
 
