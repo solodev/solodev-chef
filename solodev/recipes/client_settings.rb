56e407468ea487be41cc0bf0a616e076a70063a6
smmadara <smmadara@gmail.com>
2020-11-20 08:46:04 -0500

    SSOEnabled param for Solodev Cloud

--- solodev/recipes/client_settings.rb
+++ solodev/recipes/client_settings.rb
@@ -2,12 +2,14 @@ DocumentRoot = node[:install][:DocumentRoot]
 SoftwareName = node[:install][:SoftwareName]
 ClientName = node[:install][:ClientName]
 MongoHost = node[:install][:MongoHost]
+SSOEnabled = node[:install][:SSOEnabled]
 
 template '.env' do
 	not_if { ::File.exists?("#{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}/.env") }
   path "#{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}/.env"
   variables( 
-  	MongoHost: "#{MongoHost}"
+    MongoHost: "#{MongoHost}",
+    SSOEnabled: "#{SSOEnabled}"
   )
   action :create
   source 'client.env.erb'
