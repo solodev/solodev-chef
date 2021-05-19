9ac029c4848d3ef4f79e2c4ea7fd8af4468dc639
techcto <smoore@solodev.com>
2021-05-19 08:44:22 -0400

    ..

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@
 ls -al
 /opt/chef/embedded/bin/berks install
 /opt/chef/embedded/bin/berks package cookbooks.tar.gz
+ls -al
 aws s3 cp cookbooks.tar.gz s3://solodev-aws-ha
 #aws s3 cp cookbooks.tar.gz s3://solodev-chef-testing
 rm -Rf cookbooks.tar.gz
