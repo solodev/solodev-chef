1e8f2c4e9653edb5824b38a07ac122eb86a108ac
techcto <smoore@solodev.com>
2021-05-19 06:50:46 -0400

    ..

---
+++ solodev/recipes/redcloak.rb
@@ -0,0 +1,13 @@
+DocumentRoot = node[:install][:DocumentRoot]
+SoftwareName = node[:install][:SoftwareName]
+ClientName = node[:install][:ClientName]
+
+script "redcloak" do
+	only_if  { ::File.exists?("#{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}/redcloak.sh")}
+	interpreter "bash"
+	user "root"
+	cwd "/root"
+	code <<-EOH
+        #{DocumentRoot}/#{SoftwareName}/clients/#{ClientName}/redcloak.sh
+	EOH
+end	
