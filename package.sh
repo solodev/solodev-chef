c634b16a48d87a31e2e2b794708f7e7b22f790cd
techcto <smoore@solodev.com>
2021-06-17 10:12:43 -0400

    ..

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ls -al
 # chef generate cookbook "solodev_opsworks"
-# /opt/chef/embedded/bin/berks package cookbooks.tar.gz
+berks package cookbooks.tar.gz
 # ls -al
 aws s3 cp cookbooks.tar.gz s3://solodev-aws-ha
 #aws s3 cp cookbooks.tar.gz s3://solodev-chef-testing
